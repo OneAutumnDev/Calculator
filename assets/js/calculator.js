85cc9c09e93197626ca3464d858f1fe2b8008ff0
Michael Shea <oneautumndev@gmail.com>
2017-08-09 21:16:57 -0600

    All number buttons (0-9) have click event listeners that add them to the end of the numButtons array. This is the value that I want to be 'current' and displayed on the screen at any moment.

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -7,12 +7,15 @@
 //Object that stores the values of 1-9 + 0, for onkeypress events. Functionality for users to type inputs into the
     //keyboard instead of only click listeners.
 
+var current = 0; //The current value, should show on screen
+var inputDigit = ""; //The value that is being added to the current value.
 var numButtons = [];
 //when I click on 1, update the screen. I will need to take the value stored in 1, and "add" it to the value on the screen.
-var els = document.querySelector(".one");
-  els.addEventListener("click", function(){
-    alert("connected");
+var numBtn = document.querySelectorAll(".one");
+for(var i = 0; i < numBtn.length; i++){
+  numBtn[i].addEventListener("click", function(){
+    numButtons.push(this.value);
   });
-
+}
 //Currently selecting only the first class named "one"; I will need a add an event listener to all of the buttons.
 //Either by adding the event listener to a different class each time, or by creating a loop.
