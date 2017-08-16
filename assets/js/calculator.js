887e5c5e4d06675c050b912aa435029e51c48f1a
Michael Shea <oneautumndev@gmail.com>
2017-08-16 09:46:01 -0600

    Update the current value on screen every time a button is clicked. Need to remove the commas and just display the numbers.

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -8,7 +8,7 @@
     //keyboard instead of only click listeners.
 
 
-var current = document.querySelector("#screenSpan"); //The current value, should show on screen
+var current = document.querySelector(".screen span"); //The current value, should show on screen
 var inputDigit = ""; //The value that is being added to the current value.
 var numButtons = [];
 //when I click on 1, update the screen. I will need to take the value stored in 1, and "add" it to the value on the screen.
@@ -16,10 +16,11 @@ var numBtn = document.querySelectorAll(".one");
 for(var i = 0; i < numBtn.length; i++){
   numBtn[i].addEventListener("click", function(){
     numButtons.push(this.value);
+    current.textContent = numButtons.toString();
   });
 }
 //Currently selecting only the first class named "one"; I will need a add an event listener to all of the buttons.
 //Either by adding the event listener to a different class each time, or by creating a loop.
 //current.textContent = numButtons;
-numButtons.toString();
-var display = numButtons.replace(/,/g,"");
+
+// I need the conversion to string to run after we have click buttons, everytime we click a button it runs.
