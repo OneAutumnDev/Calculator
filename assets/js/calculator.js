789254e845a91a2a99604ccfff25a3083387979c
Michael Shea <oneautumndev@gmail.com>
2017-08-18 15:02:27 -0600

    undo reset

--- assets/js/calculator.js
+++ assets/js/calculator.js
@@ -7,47 +7,19 @@
 //Object that stores the values of 1-9 + 0, for onkeypress events. Functionality for users to type inputs into the
     //keyboard instead of only click listeners.
 
-var inp1 = "";
-var inpSign = "";
-var inp2 = "";
-var total = ""; //initial total. Changes when calculation run
+var display = "";
 var current = document.querySelector(".screen span"); //The current value, shown on the screen
 var numButtons = [];//Arry that number button clicks are stored in.
-var numButtons2 = [];
-var signButtons = [];
 var numBtn = document.querySelectorAll(".one"); //Selector, all elements with class of "one" (placeholder)
-var signBtn = document.querySelectorAll(".sign");
-
-
-
 //Loop through all of the buttons (querySelectorAll stores them in a pseudo Array)
 for(var i = 0; i < numBtn.length; i++){
   //At numBtn[i] (pseudo Array) add a click listener that takes an anonymous function.
   numBtn[i].addEventListener("click", function(){
     //Push(add) the value of numBtn[i] to the end of the numButtons array, each time a button is pressed.
-    if (inpSign.length === 0) {
-      numButtons.push(this.value);
-      //Update the display to show the current value, converted to a string and without the commas.
-      inp1 = numButtons.toString().replace(/,/g,"");
-      current.textContent = inp1;
-      total = inp1 + inpSign + inp2;
-    } else {
-      numButtons2.push(this.value);
-      inp2 = numButtons2.toString().replace(/,/g,"");
-      current.textContent = inp2;
-      total = inp1 + inpSign + inp2;
-    }
+    numButtons.push(this.value);
+    //Update the display to show the current value, converted to a string and without the commas.
+    display = numButtons.toString().replace(/,/g,"");
+    current.textContent = display;
+    //Is the value of display only within this function? --Probably!
   });
 }
-
-
-for(var i = 0; i < signBtn.length; i++){
-  signBtn[i].addEventListener("click", function(){
-    signButtons.push(this.value);
-    inpSign = signButtons.toString().replace(/,/g, "");
-    current.textContent = inpSign;
-    total = inp1 + inpSign + inp2;
-  });
-}
-
-//reset function: after input 1, reset to empty. Save next inputs to a new variable.
